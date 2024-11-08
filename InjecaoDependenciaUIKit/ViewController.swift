e24ecb51fac2a5d8e6ce3691189ec9fe75ca56ca
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-11-08 14:42:26 -0300

    Initial Commit

---
+++ InjecaoDependenciaUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InjecaoDependenciaUIKit
+//
+//  Created by Gabriel Vicentin Negro on 08/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
